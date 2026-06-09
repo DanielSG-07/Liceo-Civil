36facb7fff6fd821a897782a6636805133e495c4
DanielSG-07 <jdanielsanchezg7@gmail.com>
2026-06-09 19:22:01 -0400

    HashRoutes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 import Index from './components/Index';
 import ProjectList from './components/ProjectList';
 import ProjectDetail from './components/ProjectDetail';
@@ -7,14 +7,14 @@ import Authors from './components/Authors';
 
 function App() {
   return (
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
+    <HashRouter basename={process.env.PUBLIC_URL}>
       <Routes>
         <Route path="/" element={<Index />} />
         <Route path="/proyectos" element={<ProjectList />} />
         <Route path="/proyectos/:id" element={<ProjectDetail />} />
         <Route path="/autores" element={<Authors />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
